5df9041df14af7fe8a8da6dd962257970feefedf
Glenn Bucagu <52401614+g12bftd@users.noreply.github.com>
2025-07-14 16:45:19 +0200

    updated estimator

---
+++ src/gluonts/mx/model/transformer_hierarchical/_estimator.py
@@ -0,0 +1,361 @@
+from functools import partial
+from typing import List, Optional
+import logging
+
+from mxnet.gluon import HybridBlock
+import mxnet as mx
+import numpy as np
+
+from gluonts.core.component import validated
+from gluonts.dataset.common import Dataset
+from gluonts.dataset.field_names import FieldName
+from gluonts.dataset.loader import (
+    DataLoader,
+    TrainDataLoader,
+    ValidationDataLoader,
+)
+from gluonts.model.predictor import Predictor
+from gluonts.mx.batchify import batchify
+from gluonts.mx.distribution import DistributionOutput, StudentTOutput
+from gluonts.mx.model.estimator import GluonEstimator
+from gluonts.mx.model.predictor import RepresentableBlockPredictor
+from gluonts.mx.trainer import Trainer
+from gluonts.mx.util import copy_parameters, get_hybrid_forward_input_names
+from gluonts.time_feature import (
+    TimeFeature,
+    get_lags_for_frequency,
+    time_features_from_frequency_str,
+)
+from gluonts.transform import (
+    AddAgeFeature,
+    AddObservedValuesIndicator,
+    AddTimeFeatures,
+    AsNumpyArray,
+    Chain,
+    ExpectedNumInstanceSampler,
+    InstanceSampler,
+    InstanceSplitter,
+    RemoveFields,
+    SelectFields,
+    SetField,
+    TestSplitSampler,
+    Transformation,
+    ValidationSplitSampler,
+    VstackFeatures,
+)
+
+from ._network import HierarchicalTransformerPredictionNetwork, HierarchicalTransformerTrainingNetwork
+from .trans_decoder import HierarchicalTransformerDecoder
+from .trans_encoder import HierarchicalTransformerEncoder
+
+from gluonts.mx.model.deepvar_hierarchical._estimator import projection_mat
+
+logger = logging.getLogger(__name__)
+
+class HierarchicalTransformerEstimator(GluonEstimator):
+
+    @validated()
+    def __init__(
+        self,
+        freq: str,
+        prediction_length: int,
+        S: np.ndarray,
+        D: Optional[np.ndarray] = None,
+        num_encoder_layers: int = 2,
+        num_decoder_layers: int = 2,
+        num_samples_for_loss: int = 200,
+        likelihood_weight: float = 0.0,
+        CRPS_weight: float = 1.0,
+        sample_LH: bool = False,
+        coherent_train_samples: bool = True,
+        coherent_pred_samples: bool = True,
+        warmstart_epoch_frac: float = 0.0,
+        seq_axis: Optional[List[int]] = None,
+        log_coherency_error: bool = True,
+        context_length: Optional[int] = None,
+        trainer: Trainer = Trainer(),
+        dropout_rate: float = 0.1,
+        cardinality: Optional[List[int]] = None,
+        embedding_dimension: int = 20,
+        distr_output: DistributionOutput = StudentTOutput(),
+        model_dim: int = 32,
+        inner_ff_dim_scale: int = 4,
+        pre_seq: str = "dn",
+        post_seq: str = "drn",
+        act_type: str = "softrelu",
+        num_heads: int = 8,
+        scaling: bool = True,
+        lags_seq: Optional[List[int]] = None,
+        time_features: Optional[List[TimeFeature]] = None,
+        use_feat_dynamic_real: bool = False,
+        use_feat_static_cat: bool = False,
+        num_parallel_samples: int = 100,
+        train_sampler: Optional[InstanceSampler] = None,
+        validation_sampler: Optional[InstanceSampler] = None,
+        batch_size: int = 32,
+    ) -> None:
+        rank = 0
+        target_dim = len(S)
+        distr_output = LowrankMultivariateGaussianOutput(
+            dim=target_dim, rank=rank
+        )
+        super().__init__(trainer=trainer, batch_size=batch_size)
+
+        # Assert that projection is *not* being done only during training
+        assert coherent_pred_samples or (
+            not coherent_train_samples
+        ), "Cannot project only during training (and not during prediction)"
+
+
+        assert (
+            prediction_length > 0
+        ), "The value of `prediction_length` should be > 0"
+        assert (
+            context_length is None or context_length > 0
+        ), "The value of `context_length` should be > 0"
+        assert dropout_rate >= 0, "The value of `dropout_rate` should be >= 0"
+        assert (
+            cardinality is not None or not use_feat_static_cat
+        ), "You must set `cardinality` if `use_feat_static_cat=True`"
+        assert cardinality is None or all(
+            [c > 0 for c in cardinality]
+        ), "Elements of `cardinality` should be > 0"
+        assert (
+            embedding_dimension > 0
+        ), "The value of `embedding_dimension` should be > 0"
+        assert (
+            num_parallel_samples > 0
+        ), "The value of `num_parallel_samples` should be > 0"
+
+        M = projection_mat(S=S, D=D)
+        self.S = S
+        ctx = self.trainer.ctx
+        self.M = mx.nd.array(M, ctx=ctx)
+        self.num_samples_for_loss = num_samples_for_loss
+        self.likelihood_weight = likelihood_weight
+        self.CRPS_weight = CRPS_weight
+        self.log_coherency_error = log_coherency_error
+        self.coherent_train_samples = coherent_train_samples
+        self.coherent_pred_samples = coherent_pred_samples
+        self.warmstart_epoch_frac = warmstart_epoch_frac
+        self.sample_LH = sample_LH
+        self.seq_axis = seq_axis
+
+
+        self.num_encoder_layers = num_encoder_layers
+        self.num_decoder_layers = num_decoder_layers
+        self.prediction_length = prediction_length
+        self.context_length = (
+            context_length if context_length is not None else prediction_length
+        )
+        self.distr_output = distr_output
+        self.dropout_rate = dropout_rate
+        self.use_feat_dynamic_real = use_feat_dynamic_real
+        self.use_feat_static_cat = use_feat_static_cat
+        self.cardinality = cardinality if use_feat_static_cat else [1]
+        self.embedding_dimension = embedding_dimension
+        self.num_parallel_samples = num_parallel_samples
+        self.lags_seq = (
+            lags_seq
+            if lags_seq is not None
+            else get_lags_for_frequency(freq_str=freq)
+        )
+        self.time_features = (
+            time_features
+            if time_features is not None
+            else time_features_from_frequency_str(freq)
+        )
+        self.history_length = self.context_length + max(self.lags_seq)
+        self.scaling = scaling
+
+        self.encoder_config = {
+            "model_dim": model_dim,
+            "pre_seq": pre_seq,
+            "post_seq": post_seq,
+            "dropout_rate": dropout_rate,
+            "inner_ff_dim_scale": inner_ff_dim_scale,
+            "act_type": act_type,
+            "num_heads": num_heads,
+            "num_encoder_layers": num_encoder_layers,
+        }
+
+        self.decoder_config = {
+            "model_dim": model_dim,
+            "pre_seq": pre_seq,
+            "post_seq": post_seq,
+            "dropout_rate": dropout_rate,
+            "inner_ff_dim_scale": inner_ff_dim_scale,
+            "act_type": act_type,
+            "num_heads": num_heads,
+            "num_decoder_layers": num_decoder_layers
+        }
+
+        self.encoder = HierarchicalTransformerEncoder(
+            self.context_length, self.encoder_config, prefix="enc_"
+        )
+        self.decoder = HierarchicalTransformerDecoder(
+            self.prediction_length, self.decoder_config, prefix="dec_"
+        )
+        self.train_sampler = (
+            train_sampler
+            if train_sampler is not None
+            else ExpectedNumInstanceSampler(
+                num_instances=1.0, min_future=prediction_length
+            )
+        )
+        self.validation_sampler = (
+            validation_sampler
+            if validation_sampler is not None
+            else ValidationSplitSampler(min_future=prediction_length)
+        )
+
+    def create_transformation(self) -> Transformation:
+        remove_field_names = [
+            FieldName.FEAT_DYNAMIC_CAT,
+            FieldName.FEAT_STATIC_REAL,
+        ]
+        if not self.use_feat_dynamic_real:
+            remove_field_names.append(FieldName.FEAT_DYNAMIC_REAL)
+
+        return Chain(
+            [RemoveFields(field_names=remove_field_names)]
+            + (
+                [SetField(output_field=FieldName.FEAT_STATIC_CAT, value=[0.0])]
+                if not self.use_feat_static_cat
+                else []
+            )
+            + [
+                AsNumpyArray(field=FieldName.FEAT_STATIC_CAT, expected_ndim=1),
+                AsNumpyArray(
+                    field=FieldName.TARGET,
+                    # in the following line, we add 1 for the time dimension
+                    expected_ndim=1 + len(self.distr_output.event_shape),
+                ),
+                AddObservedValuesIndicator(
+                    target_field=FieldName.TARGET,
+                    output_field=FieldName.OBSERVED_VALUES,
+                ),
+                AddTimeFeatures(
+                    start_field=FieldName.START,
+                    target_field=FieldName.TARGET,
+                    output_field=FieldName.FEAT_TIME,
+                    time_features=self.time_features,
+                    pred_length=self.prediction_length,
+                ),
+                AddAgeFeature(
+                    target_field=FieldName.TARGET,
+                    output_field=FieldName.FEAT_AGE,
+                    pred_length=self.prediction_length,
+                    log_scale=True,
+                ),
+                VstackFeatures(
+                    output_field=FieldName.FEAT_TIME,
+                    input_fields=[FieldName.FEAT_TIME, FieldName.FEAT_AGE]
+                    + (
+                        [FieldName.FEAT_DYNAMIC_REAL]
+                        if self.use_feat_dynamic_real
+                        else []
+                    ),
+                ),
+            ]
+        )
+
+    def _create_instance_splitter(self, mode: str):
+        assert mode in ["training", "validation", "test"]
+
+        instance_sampler = {
+            "training": self.train_sampler,
+            "validation": self.validation_sampler,
+            "test": TestSplitSampler(),
+        }[mode]
+
+        return InstanceSplitter(
+            target_field=FieldName.TARGET,
+            is_pad_field=FieldName.IS_PAD,
+            start_field=FieldName.START,
+            forecast_start_field=FieldName.FORECAST_START,
+            instance_sampler=instance_sampler,
+            past_length=self.history_length,
+            future_length=self.prediction_length,
+            time_series_fields=[
+                FieldName.FEAT_TIME,
+                FieldName.OBSERVED_VALUES,
+            ],
+        )
+
+    def create_training_data_loader(
+        self,
+        data: Dataset,
+        **kwargs,
+    ) -> DataLoader:
+        input_names = get_hybrid_forward_input_names(
+            HierarchicalTransformerTrainingNetwork
+        )
+        instance_splitter = self._create_instance_splitter("training")
+        return TrainDataLoader(
+            dataset=data,
+            transform=instance_splitter + SelectFields(input_names),
+            batch_size=self.batch_size,
+            stack_fn=partial(batchify, ctx=self.trainer.ctx, dtype=self.dtype),
+            **kwargs,
+        )
+
+    def create_validation_data_loader(
+        self,
+        data: Dataset,
+        **kwargs,
+    ) -> DataLoader:
+        input_names = get_hybrid_forward_input_names(
+            HierarchicalTransformerTrainingNetwork
+        )
+        instance_splitter = self._create_instance_splitter("validation")
+        return ValidationDataLoader(
+            dataset=data,
+            transform=instance_splitter + SelectFields(input_names),
+            batch_size=self.batch_size,
+            stack_fn=partial(batchify, ctx=self.trainer.ctx, dtype=self.dtype),
+        )
+
+    def create_training_network(self) -> HierarchicalTransformerTrainingNetwork:
+        return HierarchicalTransformerTrainingNetwork(
+            encoder=self.encoder,
+            decoder=self.decoder,
+            history_length=self.history_length,
+            context_length=self.context_length,
+            prediction_length=self.prediction_length,
+            distr_output=self.distr_output,
+            cardinality=self.cardinality,
+            embedding_dimension=self.embedding_dimension,
+            lags_seq=self.lags_seq,
+            scaling=self.scaling,
+        )
+
+    def create_predictor(
+        self, transformation: Transformation, trained_network: HybridBlock
+    ) -> Predictor:
+        prediction_splitter = self._create_instance_splitter("test")
+
+        prediction_network = HierarchicalTransformerPredictionNetwork(
+            encoder=self.encoder,
+            decoder=self.decoder,
+            history_length=self.history_length,
+            context_length=self.context_length,
+            prediction_length=self.prediction_length,
+            distr_output=self.distr_output,
+            cardinality=self.cardinality,
+            embedding_dimension=self.embedding_dimension,
+            lags_seq=self.lags_seq,
+            scaling=self.scaling,
+            num_parallel_samples=self.num_parallel_samples,
+        )
+
+        copy_parameters(trained_network, prediction_network)
+
+        return RepresentableBlockPredictor(
+            input_transform=transformation + prediction_splitter,
+            prediction_net=prediction_network,
+            batch_size=self.batch_size,
+            prediction_length=self.prediction_length,
+            ctx=self.trainer.ctx,
+        )
+
